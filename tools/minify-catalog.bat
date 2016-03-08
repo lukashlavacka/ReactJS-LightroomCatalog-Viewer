499aac91663c80237f01c7af9a2472075ecbb493
Lukas Hlavacka <lukas.hlavacka@gmail.com>
2016-03-08 01:06:39 -0500

    Updating js/css to compiled version

--- tools/minify-catalog.bat
+++
@@ -1,7 +0,0 @@
-Setlocal EnableDelayedExpansion
-for %%F in (*.lrcat) do (
-	set name=%%~nF-minified%%~xF
-	copy "%%F" "!name!"
-	start "" /wait sqlite3-win.exe "!name!" ".read minify-catalog.sql"
-)
-endlocal
